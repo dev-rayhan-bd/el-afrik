eaef21ca222069d24b80c517d07fa627f074530b
rayhan108 <raihanshorker56@gmail.com>
2026-01-02 15:39:04 +0600

    initial project setup

---
+++ src/app.ts
@@ -0,0 +1,49 @@
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+
+import cookieParser from 'cookie-parser'; 
+
+
+// import notFound from './app/middleware/notFound';
+// import { stripeWebhookHandler } from './app/webhook/webhook.stripe';
+import path from 'path';
+// import router from './app/routes/index';
+// import globalErrorHandler from './app/middleware/globalErrorHandler';
+// import notFound from './app/middleware/notFound';
+import morgan from 'morgan';
+
+const app: Application = express();
+//parsers
+app.use(express.json());
+app.use(cookieParser());
+
+// stripe webhook
+// app.post(
+//   '/webhook/stripe',
+//   express.raw({ type: 'application/json' }),
+//   stripeWebhookHandler, 
+// );
+// app.use("/uploads", express.static(path.resolve(process.cwd(), "uploads")));
+app.use('/uploads', express.static(path.join(__dirname, '../uploads')));
+app.use(
+  cors({
+    origin: [
+
+      'http://10.10.20.13:5000',
+      'http://10.10.20.13:3000',
+      'http://localhost:5175',
+      'http://localhost:5173',
+      'http://localhost:5174',
+    ],
+     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+    credentials: true,
+  }),
+);
+app.use(morgan("dev"))
+// app.use('/api/v1', router);
+app.get('/', (req: Request, res: Response) => {
+  res.send('Rot Server is Running...');
+});
+// app.use(globalErrorHandler);
+// app.use(notFound);
+export default app;
---
+++ src/app/config/index.ts
@@ -0,0 +1,26 @@
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config({ path: path.join((process.cwd(), '.env')) });
+
+export default {
+  NODE_ENV: process.env.NODE_ENV,
+  port: process.env.PORT,
+  database_url: process.env.DATABASE_URL,
+  server_url: process.env.SERVER_URL,
+  frontend_url: process.env.FRONTEND_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_access_expires_in: process.env.JWT_ACCESS_EXPIRES_IN,
+  jwt_refresh_expires_in: process.env.JWT_REFRESH_EXPIRES_IN,
+  smtp_from: process.env.SMTP_FROM,
+  SMTP_USER: process.env.SMTP_USER,
+  SMTP_PASS: process.env.APP_PASSWARD,
+  stripe_secret_key: process.env.STRIPE_SECRET_KEY,
+  webhook_secret_key: process.env.WEBHOOK_SECRET_KEY,
+  aws_access_key: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_secret_key: process.env.AWS_SECRET_ACCESS_KEY,
+  aws_bucket_name: process.env.AWS_BUCKET_NAME,
+  aws_region: process.env.AWS_REGION,
+};
