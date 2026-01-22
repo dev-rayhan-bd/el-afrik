9722488721bee06a578fa151b19430640e77c22f
rayhan108 <raihanshorker56@gmail.com>
2026-01-22 10:53:27 +0600

    add cors

--- src/app.ts
+++ src/app.ts
@@ -34,6 +34,7 @@ app.use(
       'http://localhost:5173',
       'http://localhost:5174',
       'http://localhost:3000',
+      'https://el-afrik.vercel.app',
     ],
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
