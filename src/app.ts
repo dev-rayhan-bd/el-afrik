e1d4a361a149130ec59d03317564e6090871be7c
rayhan108 <raihanshorker56@gmail.com>
2026-02-01 14:22:28 +0600

    add live ip to cors

--- src/app.ts
+++ src/app.ts
@@ -14,20 +14,47 @@ const app: Application = express();
 
 
 
+// app.use(
+//   cors({
+//     origin: [
+//       'http://10.10.20.13:5000',
+//       'http://10.10.20.13:3000',
+//       'http://localhost:5175',
+//       'http://localhost:5173',
+//       'http://localhost:5174',
+//       'http://localhost:3000',
+//       'https://el-afrik.vercel.app',
+//       'http://15.223.245.199',
+//       'http://15.223.245.199:3000',
+//     ],
+//     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+//     credentials: true,
+//   })
+// );
 app.use(
   cors({
-    origin: [
-      'http://10.10.20.13:5000',
+    origin: (origin, callback) => {
+      const allowedOrigins = [
+        'http://15.223.245.199:3000',
+        'http://15.223.245.199',
+        'http://localhost:3000',
+        'http://localhost:5173',
+        'https://el-afrik.vercel.app',
+        'http://10.10.20.13:5000',
       'http://10.10.20.13:3000',
       'http://localhost:5175',
       'http://localhost:5173',
-      'http://localhost:5174',
-      'http://localhost:3000',
-      'https://el-afrik.vercel.app',
-      'http://15.223.245.199',
-      'http://15.223.245.199:3000',
-    ],
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
+       'http://localhost:5174',
+       'http://localhost:3000',
+        
+      ];
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     credentials: true,
   })
 );
