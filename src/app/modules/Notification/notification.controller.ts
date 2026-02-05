783cc454b4da4dbdcc22b3ff3ee8d3a51fe71a5e
rayhan108 <raihanshorker56@gmail.com>
2026-02-05 11:55:59 +0600

    added pagination on get my notification

--- src/app/modules/Notification/notification.controller.ts
+++ src/app/modules/Notification/notification.controller.ts
@@ -5,21 +5,55 @@ import httpStatus from 'http-status';
 import { NotificationModel } from './notification.model';
 import AppError from '../../errors/AppError';
 
+// const getMyNotifications = catchAsync(async (req: Request, res: Response) => {
+//   const userId = req.user.userId;
+
+
+//   const result = await NotificationModel.find({ user: userId })
+//     .sort({ createdAt: -1 });
+
+//   sendResponse(res, {
+//     statusCode: httpStatus.OK,
+//     success: true,
+//     message: 'Notifications retrieved successfully',
+//     data: result,
+//   });
+// });
+// src/app/modules/Notification/notification.controller.ts
+
 const getMyNotifications = catchAsync(async (req: Request, res: Response) => {
   const userId = req.user.userId;
+  
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
 
 
-  const result = await NotificationModel.find({ user: userId })
-    .sort({ createdAt: -1 });
+  const notifications = await NotificationModel.find({ user: userId })
+    .sort({ createdAt: -1 }) 
+    .skip(skip)
+    .limit(limit);
+
+
+  const total = await NotificationModel.countDocuments({ user: userId });
+  const totalPages = Math.ceil(total / limit);
 
   sendResponse(res, {
     statusCode: httpStatus.OK,
     success: true,
     message: 'Notifications retrieved successfully',
-    data: result,
+    data: {
+      notifications,
+      pagination: {
+        page,
+        limit,
+        total,
+        totalPages,
+      },
+    },
   });
 });
-
 
 const markAsRead = catchAsync(async (req: Request, res: Response) => {
   const userId = req.user.userId;
